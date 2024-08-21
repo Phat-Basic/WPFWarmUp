d08193eb14fd1488b09ad5354430cec34800d6ab
Phat-Basic <phattranlikedata@gmail.com>
2024-08-21 10:42:00 +0700

    up date 58 60

---
+++ OveralDesktopUI/Form6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trang60
+{
+    public partial class frmCheckListBox : Form
+    {
+        public frmCheckListBox()
+        {
+            InitializeComponent();
+        }
+
+        private void clbMonHoc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckedListBox.CheckedItemCollection items;
+            items = this.clbMonHoc.CheckedItems;
+            string s = "";
+            foreach (object ob in items)
+            {
+                s += ob.ToString() + ",";
+            }
+            MessageBox.Show("Danh sach cac mon hoc: " + s);
+        }
+    }
+}
