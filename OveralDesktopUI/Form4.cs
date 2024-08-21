d2f3acebbfb07bbe55367db78de68f2fd8e4308f
Phat-Basic <phattranlikedata@gmail.com>
2024-08-21 10:37:54 +0700

    update 52 56

---
+++ OveralDesktopUI/Form4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trang56
+{
+    public partial class frmRadioButton : Form
+    {
+        public frmRadioButton()
+        {
+            InitializeComponent();
+        }
+
+        private void rbNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.rbNam.Checked)
+            {
+                MessageBox.Show("Gioi tinh cua ban la: " + rbNam.Text);
+            }
+        }
+
+        private void rbNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.rbNu.Checked)
+            {
+                MessageBox.Show("Gioi tinh cua ban la: " + rbNu.Text);
+            }
+        }
+    }
+}
